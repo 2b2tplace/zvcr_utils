81f0dff9bc0f8cb2eb23c2ca2aaa9c540ce5fa6a
crxyne <libc.so.6_libc_start_main@proton.me>
2026-06-28 18:49:19 +0200

    properly wait in enqueue to prevent ram usage growing out of control; use c++23 invoke_result_t instead of result_of_t

--- zvcr_utils/src/zvcr_utils/util/thread_pool.hpp
+++ zvcr_utils/src/zvcr_utils/util/thread_pool.hpp
@@ -1,52 +1,64 @@
 #pragma once
 
-#include <vector>
-#include <queue>
-#include <memory>
-#include <thread>
-#include <mutex>
 #include <condition_variable>
-#include <future>
 #include <functional>
+#include <future>
+#include <mutex>
+#include <queue>
+#include <semaphore>
 #include <stdexcept>
+#include <thread>
+#include <vector>
 
 namespace zvcr {
 
     class ThreadPool {
     public:
+        explicit ThreadPool(size_t threads, size_t maxQueueSize);
+
         explicit ThreadPool(size_t threads);
 
         ThreadPool();
 
         template<class F, class... Args>
-        std::future<std::result_of_t<F(Args...)>> enqueue(F &&f, Args &&...args) {
-            using return_type = std::result_of_t<F(Args...)>;
+        auto enqueue(F&& f, Args&&... args) -> std::future<std::invoke_result_t<F, Args...>> {
+            using return_type = std::invoke_result_t<F, Args...>;
 
-            auto task = std::make_shared<std::packaged_task<return_type()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
+            slots.acquire();
 
-            std::future<return_type> res = task->get_future();
+            auto task = std::make_shared<std::packaged_task<return_type()>>(
+                std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+            );
+
+            auto res = task->get_future();
             {
                 std::unique_lock lock(queueMutex);
-                if (stop) throw std::runtime_error("enqueue on stopped ThreadPool");
-
-                tasks.emplace([task]{ (*task)(); });
+                if (stop) {
+                    slots.release();
+                    throw std::runtime_error("enqueue on stopped ThreadPool");
+                }
+                tasks.emplace([this, task] {
+                    (*task)();
+                    slots.release(); // open a slot for the next producer
+                });
             }
             condition.notify_one();
             return res;
         }
 
         [[nodiscard]]
-        size_t queueSize() const;
+        auto queueSize() const -> size_t;
 
         ~ThreadPool();
 
-        void shutdown();
+        auto shutdown() -> void;
     private:
-        void init(size_t threads);
+        auto init(size_t threads) -> void;
 
+        std::counting_semaphore<> slots;
         std::vector<std::thread> workers;
         std::queue<std::function<void()>> tasks;
-        std::mutex queueMutex;
+        mutable std::mutex queueMutex;
         std::condition_variable condition;
         bool stop;
     };
