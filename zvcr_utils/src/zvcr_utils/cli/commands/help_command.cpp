3553dc1fe0c82ab47ca6ea8517f51998a719329b
crxyne <libc.so.6_libc_start_main@proton.me>
2026-07-01 14:16:36 +0200

    show help page on parameter syntax only with regular -help instead of -help command

--- zvcr_utils/src/zvcr_utils/cli/commands/help_command.cpp
+++ zvcr_utils/src/zvcr_utils/cli/commands/help_command.cpp
@@ -3,20 +3,20 @@
 namespace zvcr {
     auto HelpCommand::execute(const CommandLine &cli) -> ErrorMessage {
         if (cli.parser.remaining.empty()) {
+            cli.log<mc::INFO>("Basic parameter syntax:");
+            cli.log<mc::INFO>("    -parameter=value");
+            cli.log<mc::INFO>("    -parameter='string value with spaces'");
+            cli.log<mc::INFO>("    -flag");
+            cli.log<mc::INFO>("Optional parameters are denoted with []:");
+            cli.log<mc::INFO>("    [-optional=value]");
+            cli.log<mc::INFO>("Variadic parameters are denoted with [...]:");
+            cli.log<mc::INFO>("    -parameters=values[...] (example: -parameters=value1 value2 value3)");
+
             for (const auto &command : cli.commands) {
                 cli.log<mc::INFO>("{}", command->explain().at(0));
             }
             return {};
         }
-        cli.log<mc::INFO>("Basic parameter syntax:");
-        cli.log<mc::INFO>("    -parameter=value");
-        cli.log<mc::INFO>("    -parameter='string value with spaces'");
-        cli.log<mc::INFO>("    -flag");
-        cli.log<mc::INFO>("Optional parameters are denoted with []:");
-        cli.log<mc::INFO>("    [-optional=value]");
-        cli.log<mc::INFO>("Variadic parameters are denoted with [...]:");
-        cli.log<mc::INFO>("    -parameters=values[...] (example: -parameters=value1 value2 value3)");
-
         const auto &commandName = cli.parser.remaining.at(0);
         for (const auto &command : cli.commands) {
             if (command->name() != commandName) continue;
